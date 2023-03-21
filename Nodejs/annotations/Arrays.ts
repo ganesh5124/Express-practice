601d5b5fc736eb26e6d612d6a906a279e54aa637
Pepakayala Veera Ganesh Kumar <pepakayalaveeraganeshkumar@Pepakayalas-MacBook-Air.local>
2023-03-21 21:58:21 +0530

    added arrays of typescript

--- Nodejs/annotations/Arrays.ts
+++ Nodejs/annotations/Arrays.ts
@@ -1,13 +1,56 @@
-const carMakers = ['ford','toyota'];
+const carMakers = ['ford', 'toyota'];
 const dates = [new Date(), new Date()];
 
-const carByMale:string[][] = [];
+const carByMale: string[][] = [];
 
 const mod = carMakers[0];
 const myCar = carMakers.pop();
 
-carMakers.map((car:string): string => { 
+carMakers.map((car: string): string => {
     return car.toUpperCase();
 })
 
-const importantDates = [new Date(), 'hello',78];
+const importantDates = [new Date(), 'hello', 78];
+
+
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summar(): string {
+        return `Some summaery ${this.sugar}`
+    }
+}
+type Drink = [string, boolean, number]
+const pepsi: [string, number, boolean, null] = ['brown', 40, true, null];
+
+const sprite: Drink = ['clear', false, 45];
+
+const oldCivic = {
+    name: 'civic',
+    year: 2000,
+    broken: true
+};
+
+interface Vehicle {
+    name: string,
+    year: number,
+
+}
+
+
+
+const printVehicle = (vehicle: Vehicle): void => {
+
+    console.log(vehicle.year);
+
+}
+
+printVehicle(oldCivic);
+
+
+
+
+
+
+
